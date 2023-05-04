05e0eaae047f711ca4eb69dea4d9d6de3fa7f564
ElfinWeb <mr.hawl1227@gmail.com>
2023-05-04 23:22:38 +0430

    050420232322P

--- src/App.vue
+++ src/App.vue
@@ -4,16 +4,34 @@
       <div class="row">
         <h4>Account</h4>
         <div class="input-group input-group-icon">
-          <input type="text" placeholder="Full Name" />
+          <input
+            type="text"
+            placeholder="Full Name"
+            @focusout="checkValidation($event, 'Full Name', fullName)"
+            v-model="fullName"
+          />
           <div class="input-icon"><i class="fa fa-user"></i></div>
+          <p class="error-title" v-if="!fullName"></p>
         </div>
         <div class="input-group input-group-icon">
-          <input type="email" placeholder="Email Adress" />
+          <input
+            type="email"
+            placeholder="Email Adress"
+            @focusout="checkValidation($event, 'Email', email)"
+            v-model="email"
+          />
           <div class="input-icon"><i class="fa fa-envelope"></i></div>
+          <p class="error-title" v-if="!email"></p>
         </div>
         <div class="input-group input-group-icon">
-          <input type="password" placeholder="Password" />
+          <input
+            type="password"
+            placeholder="Password"
+            @focusout="checkValidation($event, 'Password', password)"
+            v-model="password"
+          />
           <div class="input-icon"><i class="fa fa-key"></i></div>
+          <p class="error-title" v-if="!password"></p>
         </div>
       </div>
       <div class="row">
@@ -66,6 +84,31 @@ import BaseCard from "./components/UI/BaseCard.vue";
 export default {
   components: {
     BaseCard,
+  },
+  data() {
+    return {
+      fullName: "",
+      email: "",
+      password: "",
+      gender: "",
+      terms: false,
+      birthDay: "",
+      birthMonth: "",
+      birthYear: "",
+    };
+  },
+  methods: {
+    checkValidation(e, inputName, value) {
+      if (!value) {
+        e.target.classList.add("error-bordered");
+        e.target.nextSibling.nextSibling.textContent = `Please enter ${inputName}`;
+        console.log(inputName);
+        console.log(value);
+      } else {
+        e.target.classList.remove("error-bordered");
+        e.target.nextSibling.nextSibling.textContent = "";
+      }
+    },
   },
 };
 </script>
